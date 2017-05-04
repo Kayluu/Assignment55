26c94e021b63c25c99780c65338f96f92145349a
BenLuu <BenLuu@BENLUU-PC>
2017-05-04 11:56:15 -0400

    Importing Project

---
+++ WcfService4/Luu_Product.svc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+namespace WcfService4
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Luu_Product" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Luu_Product.svc or Luu_Product.svc.cs at the Solution Explorer and start debugging.
+    public class Luu_Product : ILuu_Product
+    {
+
+        public List<Product> ReturnProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            string _connectionString = WebConfigurationManager.ConnectionStrings["spring17_g9ConnectionString"].ConnectionString;
+
+            //create connection
+            SqlConnection con = new SqlConnection(_connectionString);
+
+            //create command object 
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "Select Product_ID, Name, Description, Price FROM Product";
+            cmd.Connection = con;
+
+            using (con)
+            {
+                con.Open(); //open connection
+                SqlDataReader reader = cmd.ExecuteReader(); // execute command
+                while (reader.Read()) //read data
+                {
+                    Product p = new Product();
+
+                    p.Product_ID = (int)reader["Product_ID"];
+                    p.Name = (string)reader["Name"];
+                    p.Description = (string)reader["Description"];
+
+                    products.Add(p);
+
+                }
+
+            }
+
+            // add data to the list
+
+
+            return products;
+        }
+    }
+}
+
---
+++ WcfService4/WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WcfService4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+            <asp:GridView ID="grvProduct" runat="server"></asp:GridView>
+        <asp:Button ID="btnLoad" runat="server" Text="Load Data" OnClick="btnLoad_Click"/>
+    </div>
+    </form>
+</body>
+</html>
